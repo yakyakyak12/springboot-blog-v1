d715326bfb4ffd7c263b8f58f36fffc7c0098845
0106un <0106un@naver.com>
2023-08-01 19:26:51 +0900

    글쓰기 완료

--- src/main/java/shop/mtcoding/blog/controller/BoardController.java
+++ src/main/java/shop/mtcoding/blog/controller/BoardController.java
@@ -1,22 +1,81 @@
 package shop.mtcoding.blog.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import shop.mtcoding.blog.dto.WriteDTO;
+import shop.mtcoding.blog.model.Board;
+import shop.mtcoding.blog.model.User;
+import shop.mtcoding.blog.repository.BoardRepository;
 
 @Controller
 public class BoardController {
+
+  @Autowired
+  private HttpSession session;
+
+  @Autowired
+  private BoardRepository boardRepository;
+
+  // localhost:8080?page=1
   @GetMapping({ "/", "/board" })
-  public String index() {
+  public String index(
+      @RequestParam(defaultValue = "0") Integer page,
+      HttpServletRequest request) {
+    // 1. 유효성 검사 X
+    // 2. 인증검사 X
+
+    List<Board> boardList = boardRepository.findAll(page);
+    request.setAttribute("boardList", boardList);
+    request.setAttribute("prevPage", page - 1);
+    request.setAttribute("nextPage", page + 1);
+    request.setAttribute("first", page == 0 ? true : false);
+    request.setAttribute("last", false);
     return "index";
+  }
+
+  @PostMapping("/board/save")
+  public String save(WriteDTO writeDTO) {
+    // validation check (유효성 검사)
+    if (writeDTO.getTitle() == null || writeDTO.getTitle().isEmpty()) {
+      return "redirect:/40x";
+    }
+    if (writeDTO.getContent() == null || writeDTO.getContent().isEmpty()) {
+      return "redirect:/40x";
+    }
+
+    // 인증체크
+    User sessionUser = (User) session.getAttribute("sessionUser");
+    if (sessionUser == null) {
+      return "redirect:/loginForm";
+    }
+
+    boardRepository.save(writeDTO, sessionUser.getId());
+    return "redirect:/";
   }
 
   @GetMapping("/board/saveForm")
   public String saveForm() {
+    User sessionUser = (User) session.getAttribute("sessionUser");
+    if (sessionUser == null) {
+      return "redirect:/loginForm";
+    }
     return "board/saveForm";
   }
 
-  @GetMapping("/board/1")
-  public String detailForm() {
+  // localhost:8080/board/1
+  // localhost:8080/board/50
+  @GetMapping("/board/{id}")
+  public String detail(@PathVariable Integer id) {
     return "board/detailForm";
   }
 }
--- src/main/java/shop/mtcoding/blog/model/Board.java
+++ src/main/java/shop/mtcoding/blog/model/Board.java
@@ -1,10 +1,13 @@
 package shop.mtcoding.blog.model;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -12,19 +15,19 @@ import lombok.Setter;
 
 @Setter
 @Getter
-@Table(name = "user_tb")
+@Table(name = "board_tb")
 @Entity // ddl-auto가 create
-public class User {
+public class Board {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
 
-  @Column(nullable = false, length = 20, unique = true)
-  private String username;
+  @Column(nullable = false, length = 100)
+  private String title;
+  @Column(nullable = true, length = 10000)
+  private String content;
+  private Timestamp createdAt;
 
-  @Column(nullable = false, length = 20)
-  private String password;
-
-  @Column(nullable = false, length = 20)
-  private String email;
+  @ManyToOne
+  private User user;
 }
